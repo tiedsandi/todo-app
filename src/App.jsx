4243842596b6e2edb6f7d4e45b849c4bbdbae617
tiedsandi <fachransandi@gmail.com>
2023-12-18 13:46:20 +0700

    add modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Todo from './components/todo/Todo';
+import Todo from './components/Todo';
 
 function App() {
   return (
     <>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo title='Halo' />
     </>
   );
 }
