0da18f56d27615193ca80b7d9f601c316d309fdb
tiedsandi <fachransandi@gmail.com>
2023-12-24 15:34:19 +0700

    sending post http request with firebase

--- src/routes/NewMeetup.jsx
+++ src/routes/NewMeetup.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import NewMeetupForm from '../components/meetups/new meetup form/NewMeetupForm';
 
 const NewMeetup = () => {
-  return <div>NewMeetup</div>;
+  const navigate = useNavigate();
+  const addMeetupHandler = (meetupData) => {
+    fetch('https://meets-up-a8fcf-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(() => {
+      navigate('/');
+    });
+  };
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 };
 
 export default NewMeetup;
