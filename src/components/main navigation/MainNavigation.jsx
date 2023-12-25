3858656b9dc33c4156facaa241c95c4deb8b8f71
tiedsandi <fachransandi@gmail.com>
2023-12-25 21:11:31 +0700

    show favorites and add badge

--- src/components/main navigation/MainNavigation.jsx
+++ src/components/main navigation/MainNavigation.jsx
@@ -1,8 +1,12 @@
 import { NavLink } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
+import { useContext } from 'react';
+import FavoritesContext from '../../store/favorites-contex';
 
 const MainNavigation = () => {
+  const favoriteCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -15,7 +19,9 @@ const MainNavigation = () => {
             <NavLink to='/new-meetup'>Add New Meetup</NavLink>
           </li>
           <li>
-            <NavLink to='/favorites'>My Favorites</NavLink>
+            <NavLink to='/favorites'>
+              My Favorites <span className={classes.badge}>{favoriteCtx.totalFavorites}</span>
+            </NavLink>
           </li>
         </ul>
       </nav>
--- src/routes/Favorites.jsx
+++ src/routes/Favorites.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import FavoritesContext from '../store/favorites-contex';
+import MeetupList from '../components/meetups/MeetupList';
 
 const Favorites = () => {
-  return <div>Favorites</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  content =
+    favoritesCtx.totalFavorites === 0 ? (
+      <p>You got no favorites yet. Start adding some?</p>
+    ) : (
+      <MeetupList meetups={favoritesCtx.favorites} />
+    );
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 };
 
 export default Favorites;
